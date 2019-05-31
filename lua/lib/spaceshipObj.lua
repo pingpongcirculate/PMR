c15872f617db0f0c4934a7f9cd5c9e1635275a39
banionis <banionis@TONATOS>
2019-05-31 18:48:44 +0300

    scrollshooter project advancement

--- lua/lib/spaceshipObj.lua
+++ lua/lib/spaceshipObj.lua
@@ -17,6 +17,7 @@ self.x = x
 self.y = y
 self.w = w
 self.h = h
+self.speed = 1
 self.idGen = idGen
 self.angle = angle
 self.sprite = imgClass(spritePath,self.x,self.y,self.w,self.h,self.idGen());
@@ -34,16 +35,37 @@ self.angle = a;
 self.sprite:setAngle(self.angle);
 end
 
+function spaceshipObj:setSpeed(spd)
+  self.speed = speed
+  end
+
+function spaceshipObj:getSpeed()
+  return self.speed
+  end
 
 function spaceshipObj:processMouseEvent(x,y,mbS,mbF,bulletArray)
-  if (mbS == 1 ) then
+ 
+ local newX = self.x
+ local newY = self.y
+ 
+ if (mbS == 1 ) then
    self.angle = self.angle + 0.1
    self.sprite:setAngle(self.angle)   
-   end
-  if (mbS < 0 ) then
+ end
+ 
+ if (mbS < 0 ) then
 	bulletArray[#bulletArray+1] =  bulletClass(200,470,12,12,350,200,"img/ship.png",self.idGen());
    end 
-   
-end
+ 
+ if (x > self.x) then
+   newX = newX + self.speed 
+ end  
+ 
+  if (x < self.x) then
+   newX = newX - self.speed 
+ end  
+
+ 
+ end
 
 return spaceshipObj;
